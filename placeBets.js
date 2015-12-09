0e2d82645a92a615ade1a717aec8e26bde3f95e2
jared5090 <jared.warren@live.com>
2015-12-09 18:44:22 +1300

    add working CSS and SCSS for graphs

--- placeBets.js
+++ placeBets.js
@@ -10,7 +10,6 @@ function checkBets(color) {
   }
 }
 
-
 /*
 Purpose: display a background color based on point level.
 function is designed for the css color format: rgb(0,0,0), specifically the red input.
@@ -23,12 +22,17 @@ function displayColor() {
     (colorArrays['bluePoints'] * 64) + ")" });
 }
 
-
-function resetBets() {
-  for (var i = 0; i < colorNames.length; i++) {
-    colorArrays[colorNames[i] + 'RandomLetters'] = [];
-    colorArrays[colorNames[i] + 'Bets'] = [];
-    colorArrays[colorNames[i] + 'Points'] = 0;
+function displayGraph(color) {
+  for (var i = 0; i < colorArrays[color + 'Points']; i++) {
+    if (colorArrays[color + 'Points'] === i) {
+      $('dd#' + color + 'Race').addClass('percentage-' + i);
+    }
   }
+}
+
+function resetBets(color) {
+    colorArrays[color + 'RandomLetters'] = [];
+    colorArrays[color + 'Bets'] = [];
+    colorArrays[color + 'Points'] = 0;
 }
 
--- setupBets.js
+++ setupBets.js
@@ -2,6 +2,7 @@
 var letters = ["A", "B", "C", "D"];
 var colorNames = ['red', 'green', 'blue'];
 var arrayNames = ['IDs', 'RandomLetters', 'Bets'];
+var positions = ['2nd', '3rd', '4th'];
 var colorArrays = {
   redPoints: 0,
   greenPoints: 0,
@@ -9,27 +10,41 @@ var colorArrays = {
 };
 
 //Purpose: create HTML and IDs for drop-down menus.
-function generateTags(color) {
+function createMenuRow(color) {
   var formID = "#" + color + "BetsForm";
-  var positions = ['2nd', '3rd', '4th'];
-  var raceHTML = $("#" + color + "BetsForm").html();
-
-  // each iteration: append a dropdown menu, then create and set a unique ID for it.
-  // append submit button.
+  var raceHTML = $(formID).html();
   var selectID = "";
   var selectID2 = color;
   var j = 1;
+
   for (var i = 0; i < positions.length; i++) {
+    //append a dropdown menu.
     $(formID).append(raceHTML);
-    $(formID).find('.position').last().text(positions[i]);
+
+    //create unique ID, store it in array and set it in DOM.
     selectID = selectID2.concat(j.toString());
     colorArrays[color + 'IDs'].push(selectID);
     $(formID).find('select').last().attr("id", selectID);
     j += 1;
   }
-  $(formID).append("<input type='submit' value='Place Bet'>")
+  //append form submit button.
+  $(formID).append(
+    "<td class='formColumn'>" +
+    "<input type='submit' class='betButton' value='Place Bets'>" +
+    "</td>");
 }
 
+//Purpose: create HTML for understanding drop-down menus.
+function createRankRow(color) {
+  var rank = '#' + color + 'Rank';
+  var rankHTML = $(rank).html();
+
+  for (var i = 0; i < positions.length; i++) {
+    //append ranking text.
+    $(rank).append(rankHTML);
+    $(rank).find('.position').last().text(positions[i]);
+  }
+}
 
 
 //Purpose: create array with items from redLetters; the items are in random order.
@@ -72,7 +87,8 @@ for (var i = 0; i < arrayNames.length; i++) {
 
 for (var i = 0; i < colorNames.length; i++) {
   //generateTags
-  generateTags(colorNames[i]);
+  createMenuRow(colorNames[i]);
+  createRankRow(colorNames[i]);
   //setup event handlers
   getBets(colorNames[i]);
 }
@@ -85,7 +101,11 @@ $('#checkBetsButton').on('submit', function(event) {
     console.log(key);
     console.log(colorArrays[key]);
   }
-  resetBets();
+  for (var i = 0; i < colorNames.length; i++) {
+    displayGraph(colorNames[i]);
+    resetBets(colorNames[i]);
+  }
+  
 });
      
 
